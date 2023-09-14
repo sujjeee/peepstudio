4f99dddb29fa48c79e93541d3cb2c95813faf835
Suraj <surajgupta.college@gmail.com>
2023-09-15 00:02:20 +0530

    temp commit

---
+++ src/components/atoms/FacialHair.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface FacialHairProps {
+
+}
+
+const FacialHair: React.FC<FacialHairProps> = ({ }) => {
+    return <div>FacialHair</div>
+}
+
+export default FacialHair
---
+++ src/components/atoms/Head.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface HeadProps {
+
+}
+
+const Head: React.FC<HeadProps> = ({ }) => {
+    return <div>Head</div>
+}
+
+export default Head
